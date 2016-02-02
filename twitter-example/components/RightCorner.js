edfd850b255630be37ef8ab41d238b888f8dec00
David Leonardi <david.leonardi@gmail.com>
2016-02-02 09:59:29 +0100

    Update RightCorner.js
    Removing styles from constructor

--- twitter-example/components/RightCorner.js
+++ twitter-example/components/RightCorner.js
@@ -1,20 +1,17 @@
 import React, { StyleSheet, View } from 'react-native';
 
 export default class RightCorner extends React.Component {
-  constructor(props) {
-    super(props);
-    this.styles = StyleSheet.create({
-      button: {
-        width: 100,
-        height: 50,
-        backgroundColor: 'orange',
-      },
-    });
-  }
-
   render() {
     return (
-      <View style={this.styles.button} />
+      <View style={styles.button} />
     );
   }
 }
+
+var styles = StyleSheet.create({
+  button: {
+    width: 100,
+    height: 50,
+    backgroundColor: 'orange',
+  },
+}); 
