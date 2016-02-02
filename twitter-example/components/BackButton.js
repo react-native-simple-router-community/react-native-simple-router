bc3df3485f2b71ae35a3e4e2df18a27d0627b7d7
David Leonardi <david.leonardi@gmail.com>
2016-02-02 09:58:38 +0100

    Update BackButton.js
    Move out styling from constructor

--- twitter-example/components/BackButton.js
+++ twitter-example/components/BackButton.js
@@ -1,21 +1,19 @@
 import React, { StyleSheet, Image } from 'react-native';
 
 export default class BackButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.styles = StyleSheet.create({
-      backButton: {
-        width: 10,
-        height: 17,
-        marginLeft: 10,
-        marginTop: 3,
-        marginRight: 10,
-      },
-    });
-  }
   render() {
     return (
-      <Image source={require('../images/back_button.png')} style={this.styles.backButton} />
+      <Image source={require('../images/back_button.png')} style={styles.backButton} />
     );
   }
 }
+
+var styles = StyleSheet.create({
+  backButton: {
+    width: 10,
+    height: 17,
+    marginLeft: 10,
+    marginTop: 3,
+    marginRight: 10,
+    },
+});
